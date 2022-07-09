9c61f2dffceb3f80ccb80d8c741ed4884017460a
noceive <nocivescience@gmail.com>
2022-07-08 22:15:38 -0400

    funciona mas bien con los radius

--- histogram_constitution.py
+++ histogram_constitution.py
@@ -1,6 +1,4 @@
-from cloup import constraint
 from manim import *
-from matplotlib.pyplot import bar
 class HistConstitucionScene(Scene):
     CONFIG={
         'axes_config':{
@@ -20,18 +18,22 @@ class HistConstitucionScene(Scene):
         axes=Axes(**self.CONFIG['axes_config']).to_edge(DOWN,buff=1)
         rectangles=self.get_histogram_bars(axes)
         self.play(Create(axes),FadeIn(rectangles))
-     
         self.wait()
     def get_histogram_bars(self,axes):
         bars=VGroup()
-        random_height=np.array([self.get_random_height() for i in range(2)])
+        random_height=np.array([self.get_random_height() for i in range(4)])
         for i,color in zip(range(self.CONFIG['axes_config']['x_range'][1]),self.CONFIG['colors']):
             bar=Rectangle(height=random_height[i],width=self.CONFIG['axes_config']['x_range'][2]+3,color=BLUE).set_fill(color,1).move_to(axes.c2p(i+1,0),DOWN)
             bars.add(bar)
         return bars
     def get_random_height(self):
-        score=np.random.choice([True,True,False])
-        if score:
-            return 1
-        else:
-            return 0
+        score=1
+        radius=1
+        while True:
+            scale=np.random.uniform(-1,1,size=2)
+            hit_radius=np.linalg.norm(scale)
+            if  hit_radius>radius:
+                return score
+            else:
+                score+=1
+                radius=np.sqrt(radius**2-hit_radius**2)
