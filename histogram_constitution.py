6ffbabb72192fe75d452c28ea3c1d477c3175937
noceive <nocivescience@gmail.com>
2022-07-08 21:46:33 -0400

    pude ingresas el np.array

--- histogram_constitution.py
+++ histogram_constitution.py
@@ -20,14 +20,18 @@ class HistConstitucionScene(Scene):
         axes=Axes(**self.CONFIG['axes_config']).to_edge(DOWN,buff=1)
         rectangles=self.get_histogram_bars(axes)
         self.play(Create(axes),FadeIn(rectangles))
+     
         self.wait()
     def get_histogram_bars(self,axes):
         bars=VGroup()
-        random_height=self.get_height()
+        random_height=np.array([self.get_random_height() for i in range(2)])
         for i,color in zip(range(self.CONFIG['axes_config']['x_range'][1]),self.CONFIG['colors']):
             bar=Rectangle(height=random_height[i],width=self.CONFIG['axes_config']['x_range'][2]+3,color=BLUE).set_fill(color,1).move_to(axes.c2p(i+1,0),DOWN)
             bars.add(bar)
         return bars
-    def get_height(self):
-        number_scores=10000
-        
+    def get_random_height(self):
+        score=np.random.choice([True,True,False])
+        if score:
+            return 1
+        else:
+            return 0
